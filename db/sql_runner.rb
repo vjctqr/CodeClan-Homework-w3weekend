b97d364f2fabb1ba1f5806db98e9c97808af82a4
Victor <victorsetlhare@gmail.com>
2020-04-18 19:37:34 +0100

    Initial Commit

---
+++ db/sql_runner.rb
@@ -0,0 +1,15 @@
+require('pg')
+
+class SqlRunner
+
+    def self.run(sql, value = [])
+        begin
+            db = PG.connect({dbname: 'cinema', host: 'localhost'})
+            db.prepare("query", sql)
+            result = db.exec_prepared("query", values)
+        ensure
+            db.close() if db != nil
+        end
+        return result
+    end
+end
